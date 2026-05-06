6dfe23ee0930a9341275ae03e636ef7ab2a96228
Jiiiee <yyuujiiie@gmail.com>
2026-05-06 22:13:51 +0800

    Add A* heuristic learning examples

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 from src.graph import Graph, astar, bfs, dfs
 
 
+Point = tuple[float, float]
+
+
+def zero_heuristic(_node: object, _goal: object) -> float:
+    return 0
+
+
+def manhattan_distance(node: Point, goal: Point) -> float:
+    return abs(node[0] - goal[0]) + abs(node[1] - goal[1])
+
+
+def euclidean_distance(node: Point, goal: Point) -> float:
+    x_distance = node[0] - goal[0]
+    y_distance = node[1] - goal[1]
+    return (x_distance**2 + y_distance**2) ** 0.5
+
+
 def build_demo_graph() -> Graph:
     graph = Graph()
     graph.add_edge("A", "B")
@@ -18,13 +35,29 @@ def build_weighted_demo_graph() -> Graph:
     return graph
 
 
+def build_coordinate_demo_graph() -> Graph:
+    graph = Graph()
+    graph.add_edge((0, 0), (1, 0), 1)
+    graph.add_edge((1, 0), (2, 0), 1)
+    graph.add_edge((2, 0), (2, 1), 1)
+    graph.add_edge((0, 0), (0, 1), 1)
+    graph.add_edge((0, 1), (1, 1), 1)
+    graph.add_edge((1, 1), (2, 1), 1)
+    return graph
+
+
 def main() -> None:
     graph = build_demo_graph()
     weighted_graph = build_weighted_demo_graph()
+    coordinate_graph = build_coordinate_demo_graph()
 
     print("BFS from A:", bfs(graph, "A"))
     print("DFS from A:", dfs(graph, "A"))
-    print("A* from A to D:", astar(weighted_graph, "A", "D", lambda _node, _goal: 0))
+    print("A* from A to D:", astar(weighted_graph, "A", "D", zero_heuristic))
+    print(
+        "A* grid path:",
+        astar(coordinate_graph, (0, 0), (2, 1), manhattan_distance),
+    )
 
 
 if __name__ == "__main__":
